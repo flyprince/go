91ceda423de6d5146c3911d186cd6ae2c8976ca4
flyprince <470892441@qq.com>
2016-08-31 11:27:58 +0800

    extest.php file commit

---
+++ extest.php
@@ -0,0 +1,98 @@
+<?php
+$this->headTitle($this->translate('Extrabux海淘教程|手把手启蒙海淘攻略,图文教程轻松学'));
+$this->headMeta()->setName('keywords', 'Extrabux海淘教程,Extrabux海淘返利教程,海淘返利攻略');
+$this->headMeta()->setName('description', 'Extrabux海淘教程以图文混编的形式向你讲述什么是海淘，如何海淘更省钱？你仅需按照教程操作仅需4步即可0门槛拿5美金，撸美国人羊毛就是那么简单，再也不用为海淘伤脑筋，Extrabux图文海淘教程助您开启海淘之旅，分分钟从菜鸟变身海淘达人。');
+?>
+
+<h3 style="height: 80px; width: 300px; background-color: #0a7e07;">dddd</h3>
+
+<div class="help-tutorial-main">
+    <div class="banner">
+            <a href="#tutorial-step1" class="step1"><span></span><strong>30秒轻松注册成为会员</strong></a>
+	            <a href="#tutorial-step2" class="step2"><span></span><strong>通过Extrabux海淘拿返利</strong></a>
+		            <a href="#tutorial-step3" class="step3"><span></span><strong>返利提现</strong></a>
+			            <a href="#tutorial-step4" class="step4"><span></span><strong>邀请好友，获奖励</strong></a>
+				        </div>
+					    <div class="tutorial-wrapper">
+
+					            <div class="clearfix" id="tutorial-step1">
+						                <div class="icon"></div>
+								            <div class="number"></div>
+									                <h2>注册Extrabux</h2>
+											            <p>只需要一个Email账户和密码就能30秒轻松注册</p>
+												                <ul>
+														                <li><i>&middot;</i><span>注册奖金：</span>新注册会员只要通过Extrabux去任意合作商家完成一笔金额超过5美金的有效订单，就能获得5美金奖励</li>
+																                <li><i>&middot;</i><span>添加联系人：</span>请添加info@extrabux.com和email@extrabux.com为邮箱联系人，避免无法收取来自Extrabux客服或者系统的邮件</li>
+																		            </ul>
+																			            </div>
+																				            <div class="clearfix" id="tutorial-step2">
+																					                <div class="icon"></div>
+																							            <div class="number"></div>
+																								                <h2>海淘购物拿返利</h2>
+																										            <p>加倍返利，促销优惠天天有。攒够＄10返利，即刻申请提现！</p>
+																											                <ul>
+																													                <li><i>&middot;</i><span>搜索功能：</span>搜索功能可以让你轻松寻找商家和产品</li>
+																															                <li><i>&middot;</i><span>商家列表：</span>可以迅速了解商家信息，以及商家当前优惠，货运和付款信息</li>
+																																	                <li><a href="javascript:;" class="more">了解详细的购物流程 <span></span></a></li>
+																																			            </ul>
+																																				                <div class="diagram">
+																																						                <span></span>
+																																								                <div class="box">
+																																										                </div>
+																																												                <ol>
+																																														                    <li class="one">选择商家购物</li>
+																																																                        <li class="two">选择国内收货地址<br />或转运公司</li>
+																																																			                    <li class="three">选择支付方式并付款</li>
+																																																					                        <li class="four">等待发货</li>
+																																																								                    <li class="five">发货后7天内<br />商家反馈返利</li>
+																																																										                    </ol>
+																																																												                </div>
+																																																														        </div>
+																																																															        <div class="clearfix" id="tutorial-step3">
+																																																																            <div class="icon"></div>
+																																																																	                <div class="number"></div>
+																																																																			            <h2>收取返利收益</h2>
+																																																																				                <p>自动跟踪订单，7天未显示请联系客服</p>
+																																																																						            <ul>
+																																																																							                    <li><i>&middot;</i><span>设置提现收款方式：</span>在账户设置里选择提现方式，目前我们支持：邮寄支票/Visa或者Master信用卡或者借记卡/PAYPAL/慈善捐助</li>
+																																																																									                    <li><a href="javascript:;" class="more">了解详细的提款方式<span></span></a></li>
+																																																																											                </ul>
+
+																																																																													            <ol>
+																																																																														                    <li>收到提现申请后，Extrabux会在5个工作日内支付给到各个渠道（此时，账户标注“已支付“），之后各渠道的处理入账时间如下：</li>
+																																																																																                    <li><span>支票：</span>2周内送到美国地址</li>
+																																																																																		                    <li><span>信用卡/借记卡：</span>2周左右（只支持Visa或Mastercard）</li>
+																																																																																				                    <li><span>Paypal：</span>48小时内入账</li>
+																																																																																						                </ol>
+																																																																																								        </div>
+																																																																																									        <div class="clearfix" id="tutorial-step4">
+																																																																																										            <div class="icon"></div>
+																																																																																											                <div class="number"></div>
+																																																																																													            <h2>邀请好友 赢取5美金</h2>
+																																																																																														                <p>还能额外获得好友第一年内返利总金额的5%</p>
+																																																																																																            <ul>
+																																																																																																	                    <li><i>&middot;</i>您可以发送推荐链接、分享到社区或使用我们提供的签名档邀请您的好友注册。注册后你们都可获得5美金的奖励资格，一旦所推荐的好友完成一笔金额超过5美金（返利不为0）的有效订单，推荐奖励就可以被激活了。并且在第一年当您的朋友下单购物并成功获取返利后您还可获得TA返利的5%
+																																																																																																			                    </li>
+																																																																																																					                    <li><a href="/users/referral">如何邀请好友 &raquo;</a></li>
+																																																																																																							                    <li><i>&middot;</i>邀请好友奖励示意图</li>
+																																																																																																									                </ul>
+																																																																																																											            <div class="diagram"></div>
+																																																																																																												            </div>
+																																																																																																													        </div>
+
+																																																																																																														</div>
+
+																																																																																																														<?php
+																																																																																																														$script = <<<JS
+																																																																																																														    $(document).ready(function() {
+																																																																																																																            $('.more').click(function(){
+																																																																																																																		                $(this).parent().parent().next().toggle();
+																																																																																																																				            if($(this).hasClass('more')){
+																																																																																																																					                    $(this).removeClass('more').addClass('less');
+																																																																																																																							                }else{
+																																																																																																																									                $(this).removeClass('less').addClass('more');
+																																																																																																																											            }
+																																																																																																																												            });
+																																																																																																																	        });
+JS;
+$this->inlineScript()->appendScript($script);
